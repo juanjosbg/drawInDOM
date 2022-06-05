5a57df9a147fff806a57f05903b4b7aec8dca955
juanjosbg <juanjoseborrero95@gmail.com>
2022-06-04 22:32:22 -0500

    move player in canvas

--- Script/videoGame.js
+++ Script/videoGame.js
@@ -1,170 +1,210 @@
+
 var vp = document.getElementById("villa_platzi");
 var papel = vp.getContext("2d");
-document.addEventListener("keydown" , moverPastor);
 
-var cantidadvacas = aleatorio(0, 16);
-var cantidadpollo = aleatorio(0, 16);
-var cantidadcerdos = aleatorio(0, 16);
+var teclas = {
+    UP: 38,
+    DOWN: 40,
+    LEFT: 37,
+    RIGHT: 39
+};
 
-var movimiento = 40;
-var x = 420;
-var y = 420;
 
 var fondo = {
-  url: "/imgs/tile.png",
-  cargaOK: false
-};
-
-var pollo = {
-  url: "/imgs/pollo.png",
-  cargaOK: false
-};
-
-var perro = {
-  url: "/imgs/perro.png",
-  cargaOK: false
-};
-
-var cerdo = {
-  url: "/imgs/cerdo.png",
-  cargaOK: false
+    url: "/imgs/tile.png",
+    cargaOK: false
 };
 
 var vaca = {
-  url: "/imgs/vaca.png",
-  cargaOK: false
+    url: "/imgs/vaca.png",
+    cargaOK: false,
+    x: [],//Creo una variable X y Y para vaca y pollo en arreglo y la de Gregson la inicializo en cero
+    y: []
 };
 
-var teclas = {
-  UP: 38,
-  DOWN: 40,
-  LEFT: 37,
-  RIGHT: 39
+var cerdo = {
+    url: "/imgs/cerdo.png",
+    cargaOK: false,
+    x: [],
+    y: []
 };
 
-fondo.imagen = new Image();
+var pollo = {
+    url: "/imgs/pollo.png",
+    cargaOK: false,
+    x: [],
+    y: []
+};
+
+var Gregson = {
+    url: "/imgs/humano.png",
+    cargaOK: false,
+    x: 0,
+    y: 0
+};
+
+var cantidad = aleatorio(1, 50);
+
+fondo.imagen = new Image();//creamos objeto
 fondo.imagen.src = fondo.url;
-fondo.imagen.addEventListener("load", cargarFondo);
-
-pollo.imagen = new Image();
-pollo.imagen.src = pollo.url;
-pollo.imagen.addEventListener("load", cargarPollos);
-
-cerdo.imagen = new Image();
-cerdo.imagen.src = cerdo.url;
-cerdo.imagen.addEventListener("load", cargarCerdos);
+fondo.imagen.addEventListener("load", cargarFondo);//escuchamos el evento de carga del navegador
 
 vaca.imagen = new Image();
 vaca.imagen.src = vaca.url;
-vaca.imagen.addEventListener("load", cargarVacas);
+vaca.imagen.addEventListener("load", cargarVaca);
 
-perro.imagen = new Image();
-perro.imagen.src = perro.url;
-perro.imagen.addEventListener("load", cargarPerro);
+cerdo.imagen = new Image();
+cerdo.imagen.src = cerdo.url;
+cerdo.imagen.addEventListener("load", cargarCerdo);
 
-function cargarFondo(){
-  fondo.cargaOK = true;
-  dibujar();
+pollo.imagen = new Image();
+pollo.imagen.src = pollo.url;
+pollo.imagen.addEventListener("load", cargarPollo);
+
+Gregson.imagen = new Image();
+Gregson.imagen.src = Gregson.url;
+Gregson.imagen.addEventListener("load", cargarGregson);
+
+function cargarFondo()//cuando ocurre el evento, se llama a estas funciones para que a su vez llamen la funcion de dibujar
+{
+    fondo.cargaOK = true;
+    dibujar();
 }
 
-function cargarVacas(){
-  vaca.cargaOK = true;
-  vaca.x = [];
-  vaca.y = [];
-  for(var n=0; n < cantidadvacas; n++){
-    vaca.x[n] = aleatorio(0,7) * 60;
-    vaca.y[n] = aleatorio(0,7) * 60;
-  }
-  dibujar();
+function cargarVaca() {
+    vaca.cargaOK = true;
+    dibujar();
 }
 
-function cargarCerdos(){
-  cerdo.cargaOK = true;
-  cerdo.x = [];
-  cerdo.y = [];
-  for(var l=0; l < cantidadcerdos; l++){
-    cerdo.x[l] = aleatorio(0,7) * 60;
-    cerdo.y[l] = aleatorio(0,7) * 60;
-  }
-  dibujar();
+function cargarCerdo() {
+    cerdo.cargaOK = true;
+    dibujar();
 }
 
-function cargarPollos(){
-  pollo.cargaOK = true;
-  pollo.x = [];
-  pollo.y = [];
-  for(var e=0; e < cantidadpollo; e++){
-    pollo.x[e] = aleatorio(0,7) * 60;
-    pollo.y[e] = aleatorio(0,7) * 60;
-  }
-  dibujar();
+
+function cargarPollo() {
+    pollo.cargaOK = true;
+    dibujar();
 }
 
-function cargarPerro(){
-  perro.cargaOK = true;
-  dibujar()
+function cargarGregson() {
+    Gregson.cargaOK = true;
+    dibujar();
 }
 
-function dibujar(){
-  if(fondo.cargaOK){
-    papel.drawImage(fondo.imagen, 0, 0);
-  }
-  if(vaca.cargaOK){
-    for(v = 0; v < cantidadvacas; v++){
-      papel.drawImage(vaca.imagen, vaca.x[v], vaca.y[v]);
+
+function dibujar()//aqui se evalua cual imagen ha cargado, para que siga el orden de carga que queremos
+{
+
+    if (fondo.cargaOK) {
+        papel.drawImage(fondo.imagen, 0, 0);
     }
-  }
-  if(pollo.cargaOK){
-    for(p = 0; p < cantidadpollo; p++){
-      papel.drawImage(pollo.imagen, pollo.x[p], pollo.y[p]);
+    if (vaca.cargaOK) {
+        for (var v = 0; v < cantidad; v++) {
+            var x = (aleatorio(0, 7) * 60);
+            var y = (aleatorio(0, 7) * 60);
+            vaca.x[v] = x;
+            vaca.y[v] = y;
+            papel.drawImage(vaca.imagen, x, y);
+            console.log(cantidad);
+        }
+
     }
-  }
-  if(cerdo.cargaOK){
-    for(c = 0; c < cantidadcerdos; c++){
-      papel.drawImage(cerdo.imagen, cerdo.x[c], cerdo.y[c]);
+
+    if (cerdo.cargaOK) {
+        for (var c = 0; p < cantidad; c++) {
+            var x = (aleatorio(0, 7) * 60);
+            var y = (aleatorio(0, 7) * 60);
+            cerdo.x[p] = x;
+            cerdo.y[p] = y;
+            papel.drawImage(cerdo.imagen, x, y);
+            console.log(cantidad);
+        }
     }
-  }
-  if(perro.cargaOK){
-    papel.drawImage(perro.imagen, x, y);
-  }
+
+    if (pollo.cargaOK) {
+        for (var p = 0; p < cantidad; p++) {
+            var x = (aleatorio(0, 7) * 60);
+            var y = (aleatorio(0, 7) * 60);
+            pollo.x[p] = x;
+            pollo.y[p] = y;
+            papel.drawImage(pollo.imagen, x, y);
+            console.log(cantidad);
+        }
+    }
+
+    if (Gregson.cargaOK) {
+        var x = (aleatorio(0, 7) * 60);
+        var y = (aleatorio(0, 7) * 60);
+        Gregson.x = x;
+        Gregson.y = y;
+        papel.drawImage(Gregson.imagen, x, y);
+
+    }
 }
 
-function moverPastor(evento){
-  switch (evento.keyCode){
-    case teclas.UP:
-        y = y - movimiento;
-        if(y < -40){
-          y = 500;
-        }
-        dibujar();
-        break;
-    case teclas.DOWN:
-        y = y + movimiento
-        if(y > 500){
-           y = 0;
-        }
-        dibujar();
-        break;
-    case teclas.LEFT:
-        x = x - movimiento;
-        if(x < -40){
-           x = 500;
-        }
-        dibujar();
-        break;
-    case teclas.RIGHT:
-        x = x + movimiento;
-        if(x > 500){
-           x = 0;
-        }
-        dibujar();
-        break;
-      default:
-  }
+
+function aleatorio(min, maxi) {
+    var resultado;
+    resultado = Math.floor(Math.random() * (maxi - min + 1)) + min;
+    return resultado;
 }
 
-function aleatorio(min, maxi){
-  var resultado;
-  resultado = Math.floor(Math.random() * (maxi - min + 1)) + min;
-  return resultado;
+function moverGregson(x, y) {
+    papel.drawImage(Gregson.imagen, x, y);
 }
+
+function dibujarOtraVez()//redibujamos los objetos, solo que sin el ciclo y utilizando los numero aleatorios de la funcion dibujar
+{
+    if (fondo.cargaOK) {
+        papel.drawImage(fondo.imagen, 0, 0);
+    }
+    if (vaca.cargaOK) {
+        for (var v = 0; v < cantidad; v++) {
+            papel.drawImage(vaca.imagen, vaca.x[v], vaca.y[v]);
+            console.log(cantidad);
+        }
+    }
+    if (cerdo.cargaOK) {
+        for (var c = 0; v < cantidad; c++) {
+            papel.drawImage(cerdo.imagen, cerdo.x[v], cerdo.y[v]);
+            console.log(cantidad);
+        }
+    }
+    if (pollo.cargaOK) {
+        for (var p = 0; p < cantidad; p++) {
+            papel.drawImage(pollo.imagen, pollo.x[p], pollo.y[p]);
+            console.log(cantidad);
+        }
+    }
+}
+
+document.addEventListener("keyup", moverTecla);
+function moverTecla(evento) {
+    var movimiento = 5;
+    switch (evento.keyCode) {
+        case teclas.UP:
+            dibujarOtraVez();
+            moverGregson(Gregson.x, Gregson.y);
+            Gregson.y = Gregson.y - movimiento;
+            break;
+        case teclas.DOWN:
+            dibujarOtraVez();
+            moverGregson(Gregson.x, Gregson.y);
+            Gregson.y = Gregson.y + movimiento;
+            break;
+        case teclas.LEFT:
+            dibujarOtraVez();
+            moverGregson(Gregson.x, Gregson.y);
+            Gregson.x = Gregson.x - movimiento;
+            break;
+        case teclas.RIGHT:
+            dibujarOtraVez();
+            moverGregson(Gregson.x, Gregson.y);
+            Gregson.x = Gregson.x + movimiento;
+            break;
+        default:
+            console.log("Otra tecla");
+            break;
+    }
+}	
